5a58e1f88690ceff67d07ca1ee355fca842a956d
unknown <James@James-PC.(none)>
2012-04-08 00:47:07 -0700

    scrollareas auto-compact the scrollbars now

--- dev/com/ludamix/triad/ui/ScrollArea.hx
+++ dev/com/ludamix/triad/ui/ScrollArea.hx
@@ -12,6 +12,7 @@ class ScrollArea extends Sprite
 	
 	public var use_horizontal : Bool;
 	public var use_vertical : Bool;
+	public var scrollbar_compact : Bool;
 	
 	public var surface : DisplayObject;
 	public var scroll_mask : Sprite;
@@ -22,7 +23,7 @@ class ScrollArea extends Sprite
 	public var mask_height : Int;
 	
 	public function new(mask_width, mask_height, surface, 
-		scroll_style : ScrollableStyle, ?use_horizontal = true, ?use_vertical = true)
+		scroll_style : ScrollableStyle, ?use_horizontal = true, ?use_vertical = true, ?scrollbar_compact = true)
 	{
 		super();
 		
@@ -40,10 +41,6 @@ class ScrollArea extends Sprite
 		scroll_v = new HScrollbar6(scroll_style, mask_height, { pos:0., size:1. }, setVertical);
 		
 		scroll_v.rotation = 90;
-		scroll_v.x = mask_width;
-		scroll_v.y = 0;
-		scroll_h.x = 0;
-		scroll_h.y = mask_height - scroll_h.height;
 		
 		addChild(scroll_h);
 		addChild(scroll_v);
@@ -68,6 +65,11 @@ class ScrollArea extends Sprite
 		var mw = use_vertical && surface.height > this.mask_height ? mask_width - scroll_v.tile_w : mask_width;
 		mh = use_horizontal && surface.width > mw ? mask_height - scroll_h.tile_h : mask_height;
 		mw = use_vertical && surface.height > mh ? mask_width - scroll_v.tile_w : mask_width;
+		if (scrollbar_compact)
+		{
+			mh = Std.int(Math.min(mh, surface.height));
+			mw = Std.int(Math.min(mw, surface.width));
+		}
 		return { w:mw, h:mh };
 	}
 	
@@ -86,6 +88,10 @@ class ScrollArea extends Sprite
 		scroll_h.draw( { pos:scroll_h.highlighted.pos, size:size.w / surface.width } );
 		scroll_v.draw( { pos:scroll_v.highlighted.pos, size:size.h / surface.height } );
 			
+		scroll_v.x = size.w + scroll_v.tile_h; // the rotation does funny stuff
+		scroll_v.y = 0;
+		scroll_h.x = 0;
+		scroll_h.y = size.h;
 	}
 	
 	public function setHorizontal(pos : Float)
